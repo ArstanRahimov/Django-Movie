926d064f1abc434e957a2af7152a199cd3d96106
Arstan <rahimov.arstan@gmail.com>
2022-01-17 01:12:52 +0600

    added filtering movies by years and genres

--- movies/views.py
+++ movies/views.py
@@ -1,12 +1,24 @@
+from django import template
+from django.db.models import Q
+from django.http import JsonResponse
 from django.shortcuts import redirect, render
 from django.views.generic.base import View
 from django.views.generic import ListView, DetailView
 
-from .models import Category, Movie
+from .models import Actor, Category, Movie, Genre
 from .forms import ReviewForm
 
 
-class MoviesView(ListView):
+class GenreYear:
+    """Жанры и года выхода фильмов"""
+    def get_genres(self):
+        return Genre.objects.all()
+
+    def get_years(self):
+        return Movie.objects.filter(draft=False).values('year')
+
+
+class MoviesView(GenreYear, ListView):
     """Список фильмов"""
     model = Movie
     queryset =  Movie.objects.filter(draft=False)  # чтобы не выводились черновики
@@ -15,7 +27,7 @@ class MoviesView(ListView):
     # мы не указыаем template, так он автоматически добавляет к названию модели суффикс _list и ищет template с таким названием (movie_list.html)
 
 
-class MovieDetailView(DetailView):
+class MovieDetailView(GenreYear, DetailView):
     """Полное описание фильма"""
     model = Movie
     slug_field = 'url'  # отвечает за то, по какому полю нужно искать запись
@@ -35,3 +47,18 @@ class AddReview(View):
             form.movie = movie
             form.save()  # теперь данные из формы будут сохранены в БД
         return redirect(movie.get_absolute_url())
+
+
+class ActorView(GenreYear, DetailView):
+    """Вывод информации о актере"""
+    model = Actor
+    template_name = 'movies/actor.html'
+    slug_field = 'name'  # поле, по которому будем искать актеров
+
+
+class FilterMoviesView(GenreYear, ListView):
+    """Фильтрация фильмов"""
+    def get_queryset(self):
+        queryset = Movie.objects.filter(Q(year__in=self.request.GET.getlist('year')) | Q(genres__in=self.request.GET.getlist('genre'))
+        )
+        return queryset
